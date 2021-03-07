d336dedd001b0ad148f0b63903af4a53e6a97c27
mitrank <66832682+mitrank@users.noreply.github.com>
2021-03-07 18:34:00 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,23 @@
 #include <string>
 #include <bits/stdc++.h>
 #include <experimental/filesystem>
-#include <assert.h>
-#include <armadillo>
-#include <matplotlibcpp.h>
+#include <iterator>
+#include <queue>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/ximgproc.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/dnn/all_layers.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace std;
 using namespace cv;
-namespace plt = matplotlibcpp;
 namespace fs = std::experimental::filesystem;
 
 int process_frame(Mat left, Mat right, string name);
@@ -25,12 +33,22 @@ string DATASET_RIGHT = DATASET+"/right/";
 string DATASET_DISPARITIES = DATASET+"/disparities/";
 string DATASET_COMBINED = DATASET+"/combined/";
 
+constexpr float CONFIDENCE_THRESHOLD = 0;
+constexpr float NMS_THRESHOLD = 0.4;
+constexpr int NUM_CLASSES = 80;
+
+const cv::Scalar colors[] =
+{
+    {0, 255, 255},
+    {255, 255, 0},
+    {0, 255, 0},
+    {255, 0, 0}
+};
+const auto NUM_COLORS = sizeof(colors)/sizeof(colors[0]);
+
 int main()
 {
-	//if (__name__ == __main__)
-	//{
-		process_dataset();
-	//}
+	process_dataset();
 	return 0;
 }
 
@@ -38,9 +56,9 @@ int process_frame(Mat left, Mat right, string name)
 {
 	int kernel_size = 3;
 	cv::Mat smooth_left;
-	GaussianBlur(left, smooth_left, Size(kernel_size,kernel_size), 1.5);
+	cv::GaussianBlur(left, smooth_left, Size(kernel_size,kernel_size), 1.5);
 	cv::Mat smooth_right;
-	GaussianBlur(right, smooth_right, Size(kernel_size, kernel_size), 1.5);
+	cv::GaussianBlur(right, smooth_right, Size(kernel_size, kernel_size), 1.5);
 
 	int window_size = 9;
 	int numDisparities = 96;
@@ -68,32 +86,29 @@ int process_frame(Mat left, Mat right, string name)
 	wls_filter->setLambda(80000);
 	wls_filter->setSigmaColor(1.2);
 
-	Mat disparity_left;
+	cv::Mat disparity_left;
 	left_matcher->compute(smooth_left, smooth_right, disparity_left);
 	Mat disparity_right;
 	right_matcher->compute(smooth_right, smooth_left, disparity_right);
 
-	Mat wls_image;
+	cv::Mat wls_image;
 	wls_filter->filter(disparity_left, smooth_left, wls_image, disparity_right);
 	normalize(wls_image, wls_image, 0, 255, NORM_MINMAX);
 
-	plt::figure();
-	// auto ax = plt::Axes(fig, [0., 0., 1., 1.]);
-	// ax::set_axis_off();
-	// fig::add_axes(ax);
-	// plt::imshow(wls_image, "jet");
-	// plt::savefig(DATASET_DISPARITIES+name);
-	// plt::close();
-	create_combined_output(left, right, name);
+	cv::namedWindow("WLS Image");
+	cv::imshow("WLS Image", disparity_left);
+	cv::waitKey(0);
+	create_combined_output(left, right, "/home/mitrankshahhh/Documents/Autonomous Systems/stereo_depth_estimator/Output.png");
 }
 
 int create_combined_output(Mat left, Mat right, string name)
 {
 	cv::Mat combined;
 	hconcat(left, right, combined);
-	cv::Mat combined_image;
-	hconcat(combined, imread(DATASET_DISPARITIES+name), combined_image);
-	imwrite(DATASET_COMBINED+name, combined);
+	std::cout<<"1st hconcat done."<<endl;
+	cv::namedWindow("Disparity");
+	cv::imshow("Disparity", combined);
+	cv::waitKey(0);
 }
 
 bool startsWith(string mainStr, string toMatch)
@@ -110,41 +125,118 @@ bool startsWith(string mainStr, string toMatch)
 
 int process_dataset()
 {
-	string left_image_path = "/home/mitrankshahhh/Documents/DV/stereo_depth_estimator/img_L.png";
-//[f for f in os.listdir(DATASET_LEFT) if not f.startswith('.')]
-	// for (const auto & entry : fs::directory_iterator(DATASET_LEFT))
-	// {
-	// 	auto check_l1 = entry.DATASET_LEFT();
-	// 	auto check_l2 = '.';
-	// 	bool left_start = startsWith(check_l1, check_l2);
-	// 	if (left_start == false)
-	// 	{
-	// 		left_images.push_back(entry.DATASET_LEFT());
-	// 	}
-	// }
-	string right_image_path = "/home/mitrankshahhh/Documents/DV/stereo_depth_estimator/img_R.png";
-//[f for f in os.listdir(DATASET_RIGHT) if not f.startswith('.')]
-	// for (const auto & entry : fs::directory_iterator(path))
-	// {
-	// 	auto check_r1 = entry.DATASET_RIGHT();
-	// 	auto check_r2 = '.';
-	// 	bool right_start = startsWith(check_r1, check_r2);
-	// 	if (right_start == false)
-	// 	{
-	// 		right_images.push_back(entry.DATASET_RIGHT());
-	// 	}
-	// }
-	//assert(left_images.size() == right_images.size());
-	// int l1 = sizeof(left_images)/sizeof(left_images[0]);
-	// int r1 = sizeof(right_images)/sizeof(right_images[0]);
-	// sort(left_images, left_images+l1);
-	// sort(right_images, right_images+r1);
-	// for (int i : left_images.size())
-	// {
-	// 	auto left_image_path = DATASET_LEFT+left_images[i];
-	// 	auto right_image_path = DATASET_RIGHT+right_images[i];
-		auto left_image = imread(left_image_path, IMREAD_COLOR);
-		auto right_image = imread(right_image_path, IMREAD_COLOR);
-		process_frame(left_image, right_image, "left_image_sgbm");
-	//}
+	string left_image_path = "/home/mitrankshahhh/Documents/Autonomous Systems/stereo_depth_estimator/img_L.png";
+	string right_image_path = "/home/mitrankshahhh/Documents/Autonomous Systems/stereo_depth_estimator/img_R.png";
+  auto left_image = imread(left_image_path, IMREAD_COLOR);
+	cv::namedWindow("Left Image");
+	cv::imshow("Left Image", left_image);
+	cv::waitKey(0);
+	auto right_image = imread(right_image_path, IMREAD_COLOR);
+	cv::namedWindow("Right Image");
+	cv::imshow("Right Image", right_image);
+	cv::waitKey(0);
+	process_frame(left_image, right_image, "left_image_sgbm");
+
+	std::vector<std::string> class_names;
+  {
+      std::ifstream class_file("coco.names");
+      if (!class_file)
+      {
+          std::cerr << "failed to open coco.names\n";
+          return 0;
+      }
+      std::string line;
+      while (std::getline(class_file, line))
+          class_names.push_back(line);
+  }
+
+  auto net = cv::dnn::readNetFromDarknet("yolov4.cfg", "yolov4.weights");
+  net.setPreferableBackend(cv::dnn::DNN_BACKEND_CUDA);
+  net.setPreferableTarget(cv::dnn::DNN_TARGET_CUDA);
+  auto output_names = net.getUnconnectedOutLayersNames();
+
+  cv::Mat frame, blob;
+  frame = left_image;
+  cv::namedWindow("Input");
+  cv::imshow("Input Image", frame);
+  cv::waitKey(0);
+  std::vector<cv::Mat> detections;
+
+  cv::dnn::blobFromImage(frame);
+  auto total_start = std::chrono::steady_clock::now();
+  cv::dnn::blobFromImage(frame, blob, 0.00392, cv::Size(608, 608), cv::Scalar(), true, false, CV_32F);
+  net.setInput(blob);
+
+  auto dnn_start = std::chrono::steady_clock::now();
+  net.forward(detections, output_names);
+  auto dnn_end = std::chrono::steady_clock::now();
+
+  std::vector<int> indices[NUM_CLASSES];
+  std::vector<cv::Rect> boxes[NUM_CLASSES];
+  std::vector<float> scores[NUM_CLASSES];
+
+  for (auto& output : detections)
+  {
+      const auto num_boxes = output.rows;
+      for (int i = 0; i < num_boxes; i++)
+      {
+          auto x = output.at<float>(i, 0) * frame.cols;
+          auto y = output.at<float>(i, 1) * frame.rows;
+          auto width = output.at<float>(i, 2) * frame.cols;
+          auto height = output.at<float>(i, 3) * frame.rows;
+          cv::Rect rect(x - width/2, y - height/2, width, height);
+
+          for (int c = 0; c < NUM_CLASSES; c++)
+          {
+              auto confidence = *output.ptr<float>(i, 5 + c);
+              if (confidence >= CONFIDENCE_THRESHOLD)
+              {
+                  boxes[c].push_back(rect);
+                  scores[c].push_back(confidence);
+              }
+          }
+      }
+  }
+
+  for (int c = 0; c < NUM_CLASSES; c++)
+	{
+		cv::dnn::NMSBoxes(boxes[c], scores[c], 0.0, NMS_THRESHOLD, indices[c]);
+	}
+	for (int c= 0; c < NUM_CLASSES; c++)
+  {
+    for (size_t i = 0; i < indices[c].size(); ++i)
+    {
+        const auto color = colors[c % NUM_COLORS];
+        auto idx = indices[c][i];
+        const auto& rect = boxes[c][idx];
+        cv::rectangle(frame, cv::Point(rect.x, rect.y), cv::Point(rect.x + rect.width, rect.y + rect.height), color, 3);
+
+        std::ostringstream label_ss;
+        label_ss << class_names[c] << ": " << std::fixed << std::setprecision(2) << scores[c][idx];
+        auto label = label_ss.str();
+
+        int baseline;
+        auto label_bg_sz = cv::getTextSize(label.c_str(), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, 1, &baseline);
+        cv::rectangle(frame, cv::Point(rect.x, rect.y - label_bg_sz.height - baseline - 10), cv::Point(rect.x + label_bg_sz.width, rect.y), color, cv::FILLED);
+        cv::putText(frame, label.c_str(), cv::Point(rect.x, rect.y - baseline - 5), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(0, 0, 0));
+    }
+  }
+
+  auto total_end = std::chrono::steady_clock::now();
+
+  float inference_fps = 1000.0 / std::chrono::duration_cast<std::chrono::milliseconds>(dnn_end - dnn_start).count();
+  float total_fps = 1000.0 / std::chrono::duration_cast<std::chrono::milliseconds>(total_end - total_start).count();
+  std::ostringstream stats_ss;
+  stats_ss << std::fixed << std::setprecision(2);
+  stats_ss << "Inference FPS: " << inference_fps << ", Total FPS: " << total_fps;
+  auto stats = stats_ss.str();
+
+  int baseline;
+  auto stats_bg_sz = cv::getTextSize(stats.c_str(), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, 1, &baseline);
+  cv::rectangle(frame, cv::Point(0, 0), cv::Point(stats_bg_sz.width, stats_bg_sz.height + 10), cv::Scalar(0, 0, 0), cv::FILLED);
+  cv::putText(frame, stats.c_str(), cv::Point(0, stats_bg_sz.height + 5), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(255, 255, 255));
+
+  cv::namedWindow("output");
+  cv::imshow("output", frame);
+  cv::waitKey(0);
 }
